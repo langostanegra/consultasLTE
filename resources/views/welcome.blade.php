101eaa197e112679ee01322a03508ad737536d10
langostanegra <afhenaog@uniremingtonmanizales.edu.co>
2020-01-31 08:25:30 -0500

    Se agrega el logo de Manizales Campus y se inicia a modificar el login

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,16 +2,7 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
     	<!-- Smartsupp Live Chat script -->
-        <script type="text/javascript">
-var _smartsupp = _smartsupp || {};
-_smartsupp.key = '2fd84a73081231a6df0cc373b1ef36dae6057ef7';
-window.smartsupp||(function(d) {
-  var s,c,o=smartsupp=function(){ o._.push(arguments)};o._=[];
-  s=d.getElementsByTagName('script')[0];c=d.createElement('script');
-  c.type='text/javascript';c.charset='utf-8';c.async=true;
-  c.src='https://www.smartsuppchat.com/loader.js?';s.parentNode.insertBefore(c,s);
-})(document);
-</script>
+        <script src="{{asset('js/chat.js')}}" type="text/javascript"></script>
 
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -26,18 +17,21 @@ window.smartsupp||(function(d) {
         <div class="uniremington-logo">
             <img src="{{asset('img/uniremington_horizontal.png')}}" width="180">
         </div>
-
         
         <div class="tic-logo">
             <img src="{{asset('img/logo_tic_blanco.png')}}" width="80">
         </div>
-        
 
+        <div class="campus-logo">
+            <img src="{{asset('img/campus.png')}}" width="150">
+        </div>
+        
         <div class="mensaje-central">
             <h1>Bienvenido al portal de consultas institucionales</h1></br></br>
             <h3>Inicia sesión como estudiante o docente para acceder a nuestros servicios.</h3>
         </div>
 
+        <!--El fondo de pantalla azul-->
         <div class="area" >
             <ul class="circles">
                 <li></li>
@@ -52,7 +46,7 @@ window.smartsupp||(function(d) {
                 <li></li>
             </ul>
         </div >
-
+        
         @if (Route::has('login'))
             <div class="top-right links">
                 @auth
@@ -64,5 +58,5 @@ window.smartsupp||(function(d) {
                         @endif-->
                 @endauth
         @endif                
-    </body>    
+    </body>
 </html>
